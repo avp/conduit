d356de19380329350090410bd48ea69489d0bdde
Aakash Patel <aakpat6@gmail.com>
2015-04-13 21:43:22 -0400

    Improve boilerplate for rendering.

--- src/renderer/renderer.cpp
+++ src/renderer/renderer.cpp
@@ -50,4 +50,81 @@ Renderer::Renderer(int w, int h) {
   cout << "Resizing window to " << windowWidth << "x" << windowHeight << endl;
   SDL_SetWindowSize(win, windowWidth, windowHeight);
   SDL_SetWindowPosition(win, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+
+  ovrHmd_ConfigureTracking(hmd,
+      ovrTrackingCap_Orientation |
+      ovrTrackingCap_MagYawCorrection |
+      ovrTrackingCap_Position,
+      0);
+
+  eyeRes[0] = ovrHmd_GetFovTextureSize(hmd, ovrEye_Left,
+      hmd->DefaultEyeFov[0], 1.0);
+  eyeRes[1] = ovrHmd_GetFovTextureSize(hmd, ovrEye_Right,
+      hmd->DefaultEyeFov[1], 1.0);
+
+  fovSize.w = eyeRes[0].w + eyeRes[1].w;
+  fovSize.h = std::max(eyeRes[0].h, eyeRes[1].h);
+
+  fovTexWidth = nextPow2(fovSize.w);
+  fovTexHeight = nextPow2(fovSize.h);
+
+  for (int i = 0; i < 2; i++) {
+    ovrTex[i].OGL.Header.API = ovrRenderAPI_OpenGL;
+    ovrTex[i].OGL.Header.TextureSize.w = fovTexWidth;
+    ovrTex[i].OGL.Header.TextureSize.h = fovTexHeight;
+    /* this next field is the only one that differs between the two eyes */
+    ovrTex[i].OGL.Header.RenderViewport.Pos.x = i == 0 ? 0 : fovSize.w / 2.0;
+    ovrTex[i].OGL.Header.RenderViewport.Pos.y = 0;
+    ovrTex[i].OGL.Header.RenderViewport.Size.w = fovSize.w / 2.0;
+    ovrTex[i].OGL.Header.RenderViewport.Size.h = fovSize.h;
+    ovrTex[i].OGL.TexId = fovTex; /* both eyes will use the same texture id */
+  }
+
+  memset(&glCfg, 0, sizeof(glCfg));
+  glCfg.OGL.Header.API = ovrRenderAPI_OpenGL;
+  glCfg.OGL.Header.BackBufferSize = hmd->Resolution;
+  glCfg.OGL.Header.Multisample = 1;
+
+#ifdef OVR_OS_LINUX
+  glCfg.OGL.Disp = glXGetCurrentDisplay();
+#endif
+
+  cout << "Running in direct-hmd mode." << endl;
+
+#ifdef OVR_OS_LINUX
+  cout << "Linux: Attaching to window..." << endl;
+  ovrHmd_AttachToWindow(hmd, (void*) glXGetCurrentDrawable(), 0, 0);
+  cout << "Linux: Attached to window." << endl;
+#endif
+
+  hmdCaps = ovrHmdCap_LowPersistence | ovrHmdCap_DynamicPrediction;
+  ovrHmd_SetEnabledCaps(hmd, hmdCaps);
+
+  cout << "Configuring distortion rendering..." << endl;
+  distortCaps = ovrDistortionCap_TimeWarp | ovrDistortionCap_Overdrive;
+  if (!ovrHmd_ConfigureRendering(hmd, &glCfg.Config, distortCaps,
+        hmd->DefaultEyeFov, eyeDesc)) {
+    cerr << "Failed to configure distortion rendering." << endl;
+    exit(1);
+  }
+  cout << "Configured distortion rendering." << endl;
+
+  glEnable(GL_DEPTH_TEST);
+  glEnable(GL_CULL_FACE);
+  glEnable(GL_LIGHTING);
+  glEnable(GL_LIGHT0);
+  glEnable(GL_LIGHT1);
+  glEnable(GL_NORMALIZE);
+
+  glClearColor(0.1, 0.1, 0.1, 1);
+}
+
+unsigned int Renderer::nextPow2(unsigned int x) {
+  x--;
+  x |= x >> 1;
+  x |= x >> 2;
+  x |= x >> 4;
+  x |= x >> 8;
+  x |= x >> 16;
+  return x + 1;
 }
--- src/renderer/renderer.hpp
+++ src/renderer/renderer.hpp
@@ -2,12 +2,26 @@
 #define RENDERER_RENDERER_H_
 
 #include <GL/glew.h>
-#include <OVR_CAPI.h>
+#include <OVR_CAPI_0_5_0.h>
+#include <OVR_CAPI_GL.h>
 #include <SDL2/SDL.h>
 #include <glm/glm.hpp>
 #include <iostream>
 #include <opencv2/highgui/highgui.hpp>
 #include <unistd.h>
+
+#ifdef WIN32
+#define OVR_OS_WIN32
+
+#elif defined(__APPLE__)
+#define OVR_OS_MAC
+
+#else
+#define OVR_OS_LINUX
+#include <GL/glx.h>
+#include <X11/Xlib.h>
+
+#endif
 
 class Renderer {
   public:
@@ -18,9 +32,19 @@ class Renderer {
     SDL_GLContext ctx;
 
     ovrHmd hmd;
+    ovrSizei eyeRes[2];
+    ovrEyeRenderDesc eyeDesc[2];
+    ovrSizei fovSize;
+    ovrGLTexture ovrTex[2];
+    ovrGLTexture* prTargetTex;
 
-    int windowWidth;
-    int windowHeight;
+    int windowWidth, windowHeight;
+    int fovTexWidth, fovTexHeight;
+    unsigned int fbo, fovTex, fovDepth;
+    ovrGLConfig glCfg;
+    unsigned int distortCaps, hmdCaps;
+
+    unsigned int nextPow2(unsigned int x);
 };
 
 #endif
